9db1691b228743714d5ef14cd9dc156c3807e65a
sarah11999 <sarah.moujazi@gmx.ch>
2025-02-20 18:39:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", async (req, res) => {
                 model: "gpt-3.5-turbo",
                 messages: [{ role: "user", content: frage }],
                 temperature: 0.7,
-                max_tokens: 100
+                max_tokens: 400
             },
             {
                 headers: {
